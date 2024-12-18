cc804b1ad8b193e21dd29554445c1d27f181f840
jaeh3197 <jaeh3197@naver.com>
2024-12-18 11:37:52 +0900

    feat: ADMIN 권한 추가
    AdminRoleInterceptor 를 추가하여 관리자 권한을 확인하도록 구현하였습니다.

--- src/main/java/com/example/demo/interceptor/AdminRoleInterceptor.java
+++ src/main/java/com/example/demo/interceptor/AdminRoleInterceptor.java
@@ -1,6 +1,8 @@
 package com.example.demo.interceptor;
 
 import com.example.demo.constants.GlobalConstants;
+import com.example.demo.dto.Authentication;
+import com.example.demo.entity.Role;
 import com.example.demo.exception.UnauthorizedException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -9,24 +11,24 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+// ADMIN 인터셉터 추가
 @Component
-public class AuthInterceptor implements HandlerInterceptor {
+public class AdminRoleInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-            throws UnauthorizedException {
+    throws UnauthorizedException {
+
         HttpSession session = request.getSession(false);
         if (session == null) {
-            throw new UnauthorizedException(HttpStatus.UNAUTHORIZED, "로그인이 필요합니다.");
+            throw new UnauthorizedException(HttpStatus.UNAUTHORIZED, "세션이 끊어졌습니다.");
         }
 
-        if (session.getAttribute(GlobalConstants.USER_AUTH) == null) {
-            throw new UnauthorizedException(HttpStatus.UNAUTHORIZED, "로그인이 필요합니다.");
-        }
+        Authentication authentication = (Authentication) session.getAttribute(GlobalConstants.ADMIN_AUTH);
+        Role role = authentication.getRole();
 
-        //세션 검증 추가
-        if (session.getAttribute(GlobalConstants.ADMIN_AUTH) == null) {
-            throw new UnauthorizedException(HttpStatus.UNAUTHORIZED, "로그인이 필요합니다.");
+        if (role != Role.ADMIN) {
+            throw new UnauthorizedException(HttpStatus.UNAUTHORIZED, "admin 권한이 필요합니다.");
         }
 
         return true;
